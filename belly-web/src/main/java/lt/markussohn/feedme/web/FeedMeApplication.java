d8d73ab2cf5c9c11308ac3b8488a70ca1bcd3664
himarkussohn <jmarkus.dev@gmail.com>
2018-11-30 23:42:07 +0200

    initial commit

---
+++ belly-web/src/main/java/lt/markussohn/feedme/web/FeedMeApplication.java
@@ -0,0 +1,16 @@
+package lt.markussohn.feedme.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author himarkussohn
+ * @since 2018-11-30
+ */
+@SpringBootApplication
+public class FeedMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedMeApplication.class, args);
+    }
+}
---
+++ belly-web/src/test/java/lt/markussohn/feedme/web/FeedMeApplicationTest.java
@@ -0,0 +1,21 @@
+package lt.markussohn.feedme.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author himarkussohn
+ * @since 2018-11-30
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedMeApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
